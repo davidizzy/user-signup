cb3c30399912a685f32e28cf56791c415e21663c
davidizzy <davidizroy@gmail.com>
2017-03-31 12:36:49 -0500

    updated validations, routing, and pretty much completed the assignment

--- main.py
+++ main.py
@@ -14,51 +14,66 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-
-#TODO get the error messages to display in the UI without having the queries in the URL
-#TODO ensure validations operate correctly
-#TODO position error messages next to the form elements they refer to
-
-
 import webapp2
 import re
 import cgi
 
-def modifyFormParam(username):
-    formParams['username']=username
 
-username_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+USER_RE = re.compile(r'^[a-zA-Z0-9_-]{3,20}$')
 def validUsername(username):
-    return username_RE.match(username)
+    return username and USER_RE.match(username)
 
-password_RE = re.compile(r"^.{3,20}$")
+PASS_RE = re.compile(r'^.{3,20}$')
 def validPassword(password):
-    return password_RE.match(password)
+    return password and PASS_RE.match(password)
 
-email_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
 def validEmail(email):
-    return email_RE.match(email)
+    return not email or EMAIL_RE.match(email)
 
 
-pageHeader = """
-<!DOCTYPE html>
-<html>
-<head>
-    <style type="text/css">
-        .error {
-            color: red;
-        }
-    </style>
-</head>
-<body>
-"""
 
-# html boilerplate for the bottom of every page
-pageFooter = """
-</body>
-</html>
-"""
+def buildPage(username='', email='', uError='', pError='', vError='', eError=''):
+
+    head = """<head>
+        <style type="text/css">
+            .error {
+                color: red;
+            }
+        </style>
+    </head>
+    <h1>Signup</h1>
+    <form action="/signup" method="post">
+      <table>"""
+
+    userlabel = """<tr><td> <label for='username'>Username</label></td>"""
+    userin = """<td><input name='username' type='text' required value='{0}'/>""".format(username)
+    userEr = """<span class='error'>{0}</span></td></tr>""".format(uError)
+    userRow = userlabel + userin + userEr
+
+    passlabel = """<tr><td><label for='password' type='password'>Password</label></td>"""
+    passin = """<td><input name='password' type='password' required />"""
+    passEr = """<span class='error'>{0}</span></td></tr>""".format(pError)
+    passRow = passlabel + passin + passEr
+
+    verifylabel = """<tr><td><label for='verify' type='password'>Verify Password</label></td>"""
+    verifyin = """<td><input name='verify' type='password' required />"""
+    verifyEr = """<span class='error'>{0}</span></td></tr>""".format(vError)
+    verifyRow = verifylabel + verifyin + verifyEr
+
+    emaillabel = """<tr><td><label for='email'>Email (optional)</label></td>"""
+    emailin = """<td><input name='email' type='email' value="{0}"/>""".format(email)
+    emailEr = """<span class='error'>{0}</span></td></tr>""".format(eError)
+    emailRow = emaillabel + emailin + emailEr
+
+    foot="""
+      </table>
+      <input type="submit" />
+    </form>"""
+
+    content = head + userRow + passRow + verifyRow + emailRow + foot
+
+    return content
 
 
 class index(webapp2.RequestHandler):
@@ -67,98 +82,52 @@ class index(webapp2.RequestHandler):
 
 class signup(webapp2.RequestHandler):
     def get(self):
-        signupStart = """
-        <h1>Signup</h1>
-        <form action='/welcome' method="post">
-          <table>
-          """
+        page = buildPage()
+        self.response.write(page)
 
-        usernameRow="""
-            <tr>
-              <td>
-                <label for='username'>Username</label>
-              </td>
-              <td>
-                <input name='username' type='text' required />
-              </td>
-            </tr>
-            """
-
-        passwordRow="""
-            <tr>
-              <td>
-                <label for='password' type='password'>Password</label>
-              </td>
-              <td>
-                <input name='password' type='password' required />
-              </td>
-            </tr>
-            """
-        verifyRow="""
-            <tr>
-              <td>
-                <label for='verify' type='password'>Verify Password</label>
-              </td>
-              <td>
-                <input name='verify' type='password' required />
-              </td>
-            </tr>
-            """
-
-        emailRow="""
-            <tr>
-              <td>
-                <label for='email'>Email (optional)</label>
-              </td>
-              <td>
-                <input name='email' type='email' />
-                </td>
-              </tr>
-              """
-
-
-        signupEnd="""
-          </table>
-          <input type="submit" />
-        </form>
-        """
-
-        error = self.request.get("error")
-        if error:
-            error_esc = cgi.escape(error, quote=True)
-            error_div = '<div class="error">' + error_esc + '</div>'
-        else:
-            error_div = ''
-
-        signupHTML= signupStart + usernameRow + passwordRow + verifyRow + emailRow + signupEnd + error_div
-        content = pageHeader + signupHTML + pageFooter
-
-        self.response.write(content)
-
-
-class welcome(webapp2.RequestHandler):
     def post(self):
         username = self.request.get('username')
         password = self.request.get('password')
         verify = self.request.get('verify')
         email = self.request.get('email')
 
-        if validUsername(username) == 'False':
-            usernameErrorMessage="That is not a valid username"
-            self.redirect("/signup?error=" + usernameErrorMessage)
+        hasError = False
 
-        if validPassword(password) == 'False':
+        if not validUsername(username):
+            usernameErrorMessage="That is not a valid username"
+            hasError = True
+        else:
+            usernameErrorMessage=''
+
+        if not validPassword(password):
             passwordErrorMessage="That is not a valid password"
-            self.redirect("/signup?error=" + passwordErrorMessage)
+            hasError = True
+        else:
+            passwordErrorMessage=''
 
         if password != verify:
             verifyErrorMessage="Your passwords don't match"
-            self.redirect("/signup?error=" + verifyErrorMessage)
+            hasError = True
+        else:
+            verifyErrorMessage=''
 
-        if validEmail(email) == 'False':
+        if not validEmail(email):
             emailErrorMessage="That is not a valid email"
-            self.redirect("/signup?error=" + emailErrorMessage)
+            hasError = True
+        else:
+            emailErrorMessage=''
 
+        if hasError:
+            page = buildPage(username, email, usernameErrorMessage, passwordErrorMessage, verifyErrorMessage, emailErrorMessage)
+            self.response.write(page)
+        else:
+            self.redirect('/welcome?username={0}'.format(username))
+
+
+
+class welcome(webapp2.RequestHandler):
+    def get(self):
+        username = self.request.get('username')
 
         welcomeMessage = "<h1>Welcome, " + username + "!</h1>"
 
